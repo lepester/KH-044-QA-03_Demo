c9c868e1cb263ce7c70c415796cd7d7ca9ae59af
Arina Zaporozhets <arinazaporozhetc@gmail.com>
2021-07-02 23:23:55 +0300

    determine cost of device offering by rozetka.com.ua for exchange

--- src/main/java/rozetkaPageObjects/HomePage.java
+++ src/main/java/rozetkaPageObjects/HomePage.java
@@ -4,23 +4,9 @@ import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage extends BasePage {
-    PageHeader header;
-    PageToggledSideMenu pageToggledSideMenu;
 
     public HomePage(WebDriver driver) {
         super(driver);
-        header = new PageHeader(driver);
-        pageToggledSideMenu = new PageToggledSideMenu(driver);
-    }
-
-    @Step("Open https://rozetka.com.ua/ web-site home page")
-    public HomePage openPage() {
-        driver.get("https://rozetka.com.ua/");
-        return new HomePage(driver);
-    }
-
-    public PageHeader getHeader() {
-        return header;
     }
 
     @Step("Open https://rozetka.com.ua/pages/obmin/ web-site for determination of device`s exchange cost")
