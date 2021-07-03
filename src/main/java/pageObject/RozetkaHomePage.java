1ecf37ed0a0dabba3a76b9a7b680893de5b1e845
Arina Zaporozhets <arinazaporozhetc@gmail.com>
2021-07-03 18:36:51 +0300

    switch language

--- src/main/java/pageObject/RozetkaHomePage.java
+++ src/main/java/pageObject/RozetkaHomePage.java
@@ -3,10 +3,16 @@ package pageObject;
 import org.openqa.selenium.WebDriver;
 
 public class RozetkaHomePage extends BasePage {
+    PageHeader header;
+    PageToggledSideMenu pageToggledSideMenu;
 
     public RozetkaHomePage(WebDriver driver) {
         super(driver);
+        header = new PageHeader(driver);
+        pageToggledSideMenu = new PageToggledSideMenu(driver);
     }
 
-
+    public PageHeader getHeader() {
+        return header;
+    }
 }
---
+++ src/test/java/testClasses/switchLanguage.java
@@ -0,0 +1,23 @@
+package testClasses;
+
+import io.qameta.allure.Description;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObject.RozetkaHomePage;
+
+public class switchLanguage extends BaseTest {
+
+    @Test(groups = {"rozetkaTest"})
+    @Description("switching language of the web-site to another one")
+    public void switchLanguage() {
+        RozetkaHomePage homePage = new RozetkaHomePage(driver);
+        String oldSearchButtonText = homePage.getHeader()
+                .getSearchButtonText();
+        String newSearchButtonText = homePage.getHeader()
+                .openSideMenu()
+                .changeLanguage()
+                .getHeader()
+                .getSearchButtonText();
+        Assert.assertFalse(newSearchButtonText.equals(oldSearchButtonText));
+    }
+}
