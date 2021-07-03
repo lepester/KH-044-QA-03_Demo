9bd70e58490cebe5125b4a2b37aa7c1e6bc3904c
Lepester <slavik55350@gmail.com>
2021-07-03 20:43:15 +0300

    Set more stable locators, added assert, actions

--- src/main/java/pageObject/RozetkaTravelSearchResultsPage.java
+++ src/main/java/pageObject/RozetkaTravelSearchResultsPage.java
@@ -1,13 +1,9 @@
 package pageObject;
 
 import io.qameta.allure.Step;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.testng.Assert;
-
-import java.util.List;
 
 public class RozetkaTravelSearchResultsPage extends BasePage {
 
