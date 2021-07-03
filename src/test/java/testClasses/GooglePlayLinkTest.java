d8e99d73c9399163bfefd646818bb7f6074d8eb2
Lepester <slavik55350@gmail.com>
2021-07-03 17:40:20 +0300

    Rozetka links for mobile apps test

--- src/test/java/testClasses/GooglePlayLinkTest.java
+++ src/test/java/testClasses/GooglePlayLinkTest.java
@@ -8,8 +8,8 @@ public class GooglePlayLinkTest extends BaseTest {
     @Test
     @Description("Get to the Rozetka Shop and check mobile app link for Android")
     public void googlePlayLink() {
-        RozetkaHomePage rozetkaHomePage = new RozetkaHomePage(driver);
         driver.get(rozetkaHomePageLink);
+        RozetkaHomePage rozetkaHomePage = new RozetkaHomePage(driver);
         rozetkaHomePage.openGooglePlay();
     }
 }
