378381d117dee8936e841531a5afc0da7faa2a28
Oleh Plisko <plisko2013@gmail.com>
2021-07-03 16:41:00 +0300

    Registration Validation test

--- src/main/java/pageObject/RozetkaHomePage.java
+++ src/main/java/pageObject/RozetkaHomePage.java
@@ -1,12 +1,22 @@
 package pageObject;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class RozetkaHomePage extends BasePage {
 
+    @FindBy(css = ".main-auth__button")
+    private WebElement authButton;
     public RozetkaHomePage(WebDriver driver) {
         super(driver);
     }
 
-
+    @Step("Open Login Page")
+    public LoginPage goToLoginPage(){
+        clickableWaiter(authButton);
+        authButton.click();
+        return new LoginPage(driver);
+    }
 }
---
+++ src/test/java/testClasses/RegistrationValidationTest.java
@@ -0,0 +1,28 @@
+package testClasses;
+
+import io.qameta.allure.Description;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObject.RegisterPage;
+import pageObject.RozetkaHomePage;
+
+public class RegistrationValidationTest extends BaseTest{
+
+    @Test
+    @Description("Get to the Register and enter incorrect data")
+    public void incorrectInput(){
+        RozetkaHomePage rozetkaHomePage = new RozetkaHomePage(driver);
+        rozetkaHomePage
+                .goToLoginPage()
+                .goToRegister()
+                .inputIncorrectData(
+                        "Thomas",
+                        "West",
+                        "wasd",
+                        "azr",
+                        "qwerty-azerty");
+
+        RegisterPage registerPage = new RegisterPage(driver);
+        Assert.assertTrue(registerPage.getValidationMsg().isDisplayed());
+    }
+}
