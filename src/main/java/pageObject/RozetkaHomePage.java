ab0b999f2b4a09b515d1735d3cd152e6cf4ffa8f
Arina Zaporozhets <arinazaporozhetc@gmail.com>
2021-07-03 18:52:47 +0300

    check cost of device offering by rozetka.com.ua for exchange

--- src/main/java/pageObject/RozetkaHomePage.java
+++ src/main/java/pageObject/RozetkaHomePage.java
@@ -1,5 +1,6 @@
 package pageObject;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 
 public class RozetkaHomePage extends BasePage {
@@ -8,5 +9,9 @@ public class RozetkaHomePage extends BasePage {
         super(driver);
     }
 
-
+    @Step("Open https://rozetka.com.ua/pages/obmin/ web-site for determination of device`s exchange cost")
+    public RozetkaObminPage openRozetkaObminPage() {
+        driver.get("https://rozetka.com.ua/pages/obmin/");
+        return new RozetkaObminPage(driver);
+    }
 }
