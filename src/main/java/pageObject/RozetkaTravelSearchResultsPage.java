d821ef44a205d048b71fa7d53c571aa22fe1a83b
Lepester <slavik55350@gmail.com>
2021-07-04 11:50:26 +0300

    Removed unused locators

--- src/main/java/pageObject/RozetkaTravelSearchResultsPage.java
+++ src/main/java/pageObject/RozetkaTravelSearchResultsPage.java
@@ -8,14 +8,6 @@ public class RozetkaTravelSearchResultsPage extends BasePage {
 
     @FindBy(css = "div.s-result-item")
     private WebElement firstProduct;
-    @FindBy(xpath = "(//input[@type='number']) [1]")
-    private WebElement setMinPrice;
-    @FindBy(xpath = "(//input[@type='number']) [2]")
-    private WebElement setMaxPrice;
-    @FindBy(css = "button.price-filter__submit")
-    private WebElement submitPrice;
-    @FindBy(xpath = "//button[contains(text(), 'Цене')]")
-    private WebElement sortByPrice;
 
     public RozetkaTravelSearchResultsPage(WebDriver driver) {
         super(driver);
